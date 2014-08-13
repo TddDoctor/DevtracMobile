a639f1b2a7ed5fcc3e912ff5e929dbc3b4c2f2cc
davidebukali <david@mountbatten.net>
2014-08-13 16:35:22 +0300

    Added devtrac cloud and manual to url settings

--- platforms/android/assets/www/js/controller/controller.js
+++ platforms/android/assets/www/js/controller/controller.js
@@ -49,8 +49,8 @@ var controller = {
       //if (!localStorage.appurl) {
       //localStorage.appurl = "http://localhost/dt11";
       //localStorage.appurl = "http://192.168.38.113/dt11";
-      localStorage.appurl = "http://192.168.38.114/dt11";
-      //localStorage.appurl = "http://jenkinsge.mountbatten.net/devtracmanual";
+      //localStorage.appurl = "http://192.168.38.114/dt11";
+      localStorage.appurl = "http://jenkinsge.mountbatten.net/devtracmanual";
       //localStorage.appurl = "http://demo.devtrac.org";
       //localStorage.appurl = "http://10.0.2.2/dt11";
       //localStorage.appurl = "http://jenkinsge.mountbatten.net/devtraccloud";
@@ -510,12 +510,12 @@ var controller = {
             break;
 
 
-          case "Cloud":
+          case "cloud":
             localStorage.appurl = "http://jenkinsge.mountbatten.net/devtraccloud";
             controller.loadingMsg("Saved Url "+localStorage.appurl, 2000);
             break;
 
-          case "DevtracManual":
+          case "manual":
             localStorage.appurl = "http://jenkinsge.mountbatten.net/devtracmanual";
             controller.loadingMsg("Saved Url "+localStorage.appurl, 2000);
             break;
@@ -528,8 +528,8 @@ var controller = {
             controller.loadingMsg("Please select one url", 2000);
             break;
 
-          case "Demo":
-            localStorage.appurl = "http://demo.devtrac.org";
+          case "test":
+            localStorage.appurl = "http://test.devtrac.org";
             controller.loadingMsg("Saved Url "+localStorage.appurl, 2000);
             break;
 

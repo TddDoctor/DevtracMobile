0a59df785594a94f00fe336e05b718563ae5bd7a
davidebukali <david@mountbatten.net>
2014-09-09 10:12:23 +0300

    Untrack cordova files

--- platforms/android/bin/DevtracMobile-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/david/workspace/DevtracMobile/platforms/android/bin/DevtracMobile-debug-unaligned.apk : \
-/home/david/workspace/DevtracMobile/platforms/android/bin/DevtracMobile.ap_ \
-/home/david/workspace/DevtracMobile/platforms/android/bin/classes.dex \
-/home/david/workspace/Dmobile/platforms/android/CordovaLib/bin/classes.jar \

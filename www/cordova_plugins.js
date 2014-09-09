b1b7b4d417ef5a506e6e5c77f2e62b88a4c0d28c
davidebukali <david@mountbatten.net>
2014-09-09 17:39:37 +0300

    Updated test files

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -29,21 +29,6 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/org.apache.cordova.network-information/www/network.js",
-        "id": "org.apache.cordova.network-information.network",
-        "clobbers": [
-            "navigator.connection",
-            "navigator.network.connection"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
-        "id": "org.apache.cordova.network-information.Connection",
-        "clobbers": [
-            "Connection"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
         "id": "org.apache.cordova.dialogs.notification",
         "merges": [
@@ -248,20 +233,35 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.geolocation": "0.3.5",
-    "org.apache.cordova.network-information": "0.2.6",
     "org.apache.cordova.dialogs": "0.2.5",
     "org.apache.cordova.vibration": "0.3.6",
     "org.apache.cordova.battery-status": "0.2.5",
     "org.apache.cordova.file": "1.1.0",
     "org.apache.cordova.file-transfer": "0.4.3",
     "org.apache.cordova.camera": "0.3.1",
-    "org.apache.cordova.device": "0.2.11"
+    "org.apache.cordova.device": "0.2.11",
+    "org.apache.cordova.network-information": "0.2.11"
 }
 // BOTTOM OF METADATA
 });

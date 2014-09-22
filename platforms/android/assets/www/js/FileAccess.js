b42fc7d2481c5289147d69526c55d3796a8d640d
davidebukali <david@mountbatten.net>
2014-09-22 10:24:34 +0300

    Save report types in a file

--- platforms/android/assets/www/js/FileAccess.js
+++ platforms/android/assets/www/js/FileAccess.js
@@ -1,18 +1,18 @@
 //Phonegap code to read the site report types from the sd card
 function readTypes(fileSystem) {
-
+  
   fileSystem.root.getFile("reporttypes.txt", null, readTypesFileEntry, failreadTypes);
 }
 function readTypesFileEntry(fileEntry) {
-
+  
   fileEntry.file(readTypesFile, failreadTypes);
 }
 function readTypesFile(file) {
-
+  
   readTypesAsText(file);
 }
 function readTypesAsText(file) {
-
+  
   var reader = new FileReader();
   reader.onload = function(evt) {
     var text = evt.target.result;
@@ -22,63 +22,64 @@ function readTypesAsText(file) {
     localStorage.roadside = words[1];
     localStorage.sitevisit = words[2];
     
+    console.log("Read types "+localStorage.humaninterest + "," + localStorage.roadside+ "," +localStorage.sitevisit);
+    
   };
   reader.readAsText(file);
 }
-function failreadPass(evt) {
-
-}//username and password read ends here
+function failreadTypes(evt) {
+  console.log("cannot Read types");
+}//sitereports read ends here
 
 
 //Phonegap code to save the site report types to sd card
 function saveTypes(fileSystem) {
-
+  
   fileSystem.root.getFile("reporttypes.txt", {
     create : true,
     exclusive : false
   }, saveTypesFileEntry, failsaveTypes);
 }
 function saveTypesFileEntry(fileEntry) {
-
+  
   fileEntry.createWriter(saveTypesFileWriter, failsaveTypes);
 }
 function saveTypesFileWriter(writer) {
-
+  
   writer.onwriteend = function(evt) {
-
-    localStorage.humaninterest = data[0]['term id'];
-    localStorage.roadside = data[1]['term id'];
-    localStorage.sitevisit = data[2]['term id'];
     
+    console.log("Saved types "+localStorage.humaninterest + "," + localStorage.roadside+ "," +localStorage.sitevisit);
   };
-  var auth = $("#username").val() + "," + $("#pass").val();
+  var auth = localStorage.humaninterest + "," + localStorage.roadside+ "," +localStorage.sitevisit ;
   writer.write(auth);
 }
-function failsavePass(error) {
+function failsaveTypes(error) {
+  console.log("cannot Saved types");
   $.unblockUI();
-}//save username and passwords ends here
+}//save sitereports ends here
 
-//Phonegap code to clear passwords saved in a file on the sd card
-function clearPasswords(fileSystem) {
-
-  fileSystem.root.getFile("passwords.txt", {
+//Phonegap code to clear sitereport types saved in a file on the sd card
+function clearTypes(fileSystem) {
+  
+  fileSystem.root.getFile("reporttypes.txt", {
     create : true,
     exclusive : false
-  }, clearPassFileEntry, failclearPass);
+  }, clearTypesFileEntry, failclearTypes);
 }
-function clearPassFileEntry(fileEntry) {
-
-  fileEntry.createWriter(clearPassFileWriter, failclearPass);
+function clearTypesFileEntry(fileEntry) {
+  
+  fileEntry.createWriter(clearTypesFileWriter, failclearTypes);
 }
-function clearPassFileWriter(writer) {
-
+function clearTypesFileWriter(writer) {
+  
   writer.onwriteend = function(evt) {
-
+    
+    console.log("cleared types");
   };
   var auth = 0 + "," + 0;
   writer.write(auth);
 }
-function failclearPass(error) {
-
+function failclearTypes(error) {
+  console.log("canot clear types");
 }//clear passwords ends here
- 
+
